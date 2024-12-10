dd65171c35a8366a5809517dbc31a18005217066
jeandout <jeandout@hotmail.fr>
2024-12-10 13:00:19 +0100

    css chargelist and charge component improvements

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,14 +1,15 @@
-import { View, Text, StyleSheet, TouchableOpacity, Image, TextInput,
+import { ScrollView,
+    View, Text, StyleSheet, Image, TextInput,
     KeyboardAvoidingView, Platform
 } from "react-native";
 import Charge from "../components/Charge";
 import SelectAccount from "../components/SelectAccount";
-import {useSelector} from 'react-redux';
-import {useState} from 'react'
+import { useSelector } from 'react-redux';
+import { useState } from 'react'
 import { Button, Icon, IconElement, List, ListItem } from '@ui-kitten/components';
 
 
-export default function ListScreen({navigation}){
+export default function ListScreen({ navigation }) {
 
     //const user = useSelector((state) => state.user.value.user);
 
@@ -16,37 +17,36 @@ export default function ListScreen({navigation}){
     const selectedAccount = useSelector((state) => state.user.value.selectedAccount);
 
     //const accountObj = user.accounts.find(account=>account.name===selectedAccount);
-//let charges;
-console.log(accounts[selectedAccount])
-//if (accounts[selectedAccount].charges.length > 0){
-   const charges = accounts[selectedAccount].charges?.map((charge, i)=>{
+    //let charges;
+    console.log(accounts[selectedAccount])
+    //if (accounts[selectedAccount].charges.length > 0){
+    const charges = accounts[selectedAccount].charges?.map((charge, i) => {
         console.log(charge)
-       return <Charge key={i} name={charge.name} amount={charge.amount} date={charge.date} priority={charge.priority}/>
+        return <Charge key={i} name={charge.name} amount={charge.amount} date={charge.date} priority={charge.priority} />
     })
-//}
+    //}
 
     //amount={charge.amount} date={charge.date} priority={charge.priority}
 
     //Changer icone pour un PLUS
 
-    return(
+    return (
         <View style={styles.container}>
-            <View> 
             <SelectAccount />
-            </View>
-           
-           <View >{charges}</View>
-           <TouchableOpacity onPress={() => navigation.navigate("NewCharge")}> 
-        <Text>Ajouter une Charge</Text>
-      </TouchableOpacity>
+            <ScrollView >
+                {charges}
+            </ScrollView>
+            <Button onPress={() => navigation.navigate("NewCharge")}>
+                <Text>Ajouter une Charge</Text>
+            </Button>
         </View>
     )
 }
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        alignItems:"center",
-        justifyContent:"center",
-        backgroundColor:"#ffffff"
+    container: {
+        flex: 1,
+        gap: 15,
+        padding: 15,
     },
+   
 })
